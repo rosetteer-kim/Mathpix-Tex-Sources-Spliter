5cb184323a8ce74159e4ffd2d910d76aebef5a70
rosetteer-kim <rosetteer@gmail.com>
2024-08-03 17:30:53 +0900

    marker 위치 찾는 로직 분리

--- split_mathpix_tex_source.py
+++ split_mathpix_tex_source.py
@@ -1,41 +1,74 @@
+# Source file 구조: (쓰레기) (marker) (content) (marker) (content) ... (marker) (content) (\end{document})
+# (marker)의 종류 2024년 08월 03일 버전
+#   1. r'\\\\\n\d{1,2}\s*[\.:\)]|\n\n\d{1,2}\s*[\.:\)]|\\section\*\{\d{1,2}\.|\\section\*\{\d{1,2}\)'
+#   2. enumerate 환경 내부의 \item
+# (marker)들의 시작 위치와 끝 위치를 찾아서 (content)들을 추출
+
 import re
+from collections.abc import Callable
+from typing import Optional
+
+MarkerLocationAdjustment = Callable[[int], int]
+EnumMarkerLocations = Callable[[Optional[MarkerLocationAdjustment],
+                                Optional[MarkerLocationAdjustment]], list[tuple[int, int]]]
 
 
-def split_mathpix_tex_source(tex_source):
-    pattern = r'\\begin\{verbatim\}|\\end\{verbatim\}'
-    remove_words = re.sub(pattern, '', tex_source)
-    return split_tex_source(remove_words)
+def enum_marker_locations(text: str, marker_pattern: str) -> EnumMarkerLocations:
+    def gen_marker_locations(make_start: MarkerLocationAdjustment,
+                             make_end: MarkerLocationAdjustment) -> list[tuple[int, int]]:
+
+        matches_point = []
+        matches = re.finditer(marker_pattern, text, re.DOTALL)
+
+        for match in matches:
+            start = make_start(match.start())
+            end = make_end(match.end())
+            match_tuple = (start, end)
+            matches_point.append(match_tuple)
+
+        return matches_point
+
+    return gen_marker_locations
 
 
-def split_tex_source(tex_source):
-    contents_split = []
-    # 문제 또는 해설의 시작을 표시하는 pattern 정의
-    pattern = (r'\\\\\n\d{1,2}\s*[\.:\)]|\n\n\d{1,2}\s*[\.:\)]|\\section\*\{\d{1,2}\.|\\section\*\{\d{1,2}\)')
-    enumerate_pattern = r'\\begin{enumerate}.*?\\end{enumerate}'
-    item_pattern = r'\\item'
-    # tex_source에서 문제 또는 해설의 시작을 표시하는 pattern 위치 파악
-    matches_point = []
-    matches = re.finditer(pattern, tex_source, re.DOTALL)
-    for match in matches:
-        matches_point.append([match.start(), match.end()])
-    # tex_source에서 문제 또는 해설의 시작이 \item인 경우의 위치 파악
-    for enum_match in re.finditer(enumerate_pattern, tex_source, re.DOTALL):
+def find_marker_type1(tex_src: str):
+    marker_pattern_type1 = \
+        r'\\\\\n\d{1,2}\s*[\.:\)]|\n\n\d{1,2}\s*[\.:\)]|\\section\*\{\d{1,2}\.|\\section\*\{\d{1,2}\)'
+    gen_marker_position = enum_marker_locations(tex_src, marker_pattern_type1)
+    marker_positions = gen_marker_position(lambda x: x, lambda x: x)
+    return marker_positions
+
+
+def find_marker_type2(tex_src: str):
+    pattern_high_level = r'\\begin{enumerate}.*?\\end{enumerate}'
+    marker_pattern_type2 = r'\\item'
+    marker_positions = []
+    for enum_match in re.finditer(pattern_high_level, tex_src, re.DOTALL):
         enum_start = enum_match.start()
         enum_text = enum_match.group()
-        # enumerate 환경 내의 \item 찾기
-        for item_match in re.finditer(item_pattern, enum_text):
-            # span 값 계산 (전체 문서 기준)
-            start = enum_start + item_match.start()
-            end = enum_start + item_match.end()
-            matches_point.append([start, end])
-    # matches_point 정렬
-    matches_point.sort()
-    # 파악한 위치로 분리 저장
-    for i in range(len(matches_point) - 1):
-        contents_split.append(tex_source[matches_point[i][1]:matches_point[i + 1][0]])
-    contents_split.append(tex_source[matches_point[-1][1]:])
+        gen_marker_position = enum_marker_locations(enum_text, marker_pattern_type2)
+        marker_position = gen_marker_position(lambda x: enum_start + x, lambda x: enum_start + x)
+        marker_positions += marker_position
+    return marker_positions
 
-    for i in range(len(matches_point)):
+
+def split_mathpix_tex_source(tex_src: str):
+    pattern = r'\\begin\{verbatim\}|\\end\{verbatim\}'
+    tex_src_screened = re.sub(pattern, '', tex_src)
+    return split_tex_source(tex_src_screened)
+
+
+def split_tex_source(tex_src: str):
+    marker_positions = find_marker_type1(tex_src) + find_marker_type2(tex_src)
+    marker_positions.sort()
+
+    # 파악한 위치로 분리 저장
+    contents_split = []
+    for i in range(len(marker_positions) - 1):
+        contents_split.append(tex_src[marker_positions[i][1]:marker_positions[i + 1][0]])
+    contents_split.append(tex_src[marker_positions[-1][1]:])
+
+    for i in range(len(marker_positions)):
         # 첫 줄에서 닫힌 중괄호가 하나 더 많은 항목 수정
         contents_split[i] = remove_extra_closing_brace(contents_split[i])
         # \section*{section_title}부터 끝까지 삭제
@@ -48,10 +81,9 @@ def split_tex_source(tex_source):
     contents_split = remove_non_korean_items(filtered_contents_split)
     return contents_split
 
+
 # 첫 줄에서 닫힌 중괄호가 하나 더 많은 항목 수정
-
-
-def remove_extra_closing_brace(text):
+def remove_extra_closing_brace(text: str):
     lines = text.split('\n', 1)
     first_line = lines[0]
 
@@ -66,8 +98,8 @@ def remove_extra_closing_brace(text):
 
 
 # \section*{section_title}부터 끝까지 삭제
-def remove_section_to_end(text):
-    titles_to_remove = ['수학 영역', '공통과목', '미적분']
+def remove_section_to_end(text: str):
+    titles_to_remove = ['수학 영역', '공통과목', '미적분', '* 확인 사항']
     # \section*{section_title}부터 끝까지 선택
     section_title = '|'.join(map(re.escape, titles_to_remove))
     pattern = rf'\\section\*{{({section_title})}}.*'
@@ -76,7 +108,7 @@ def remove_section_to_end(text):
 
 
 # 지정된 단어를 포함하는 라인 삭제
-def remove_lines_with_words(text):
+def remove_lines_with_words(text: str):
     # 단어 지정
     words_to_remove = \
         ['begin{enumerate}', 'end{enumerate}', 'setcounter{enumi}', 'begin{itemize}',
@@ -93,7 +125,7 @@ def remove_lines_with_words(text):
 # 18byte 이하인 항목 삭제
 
 
-def remove_short_items(text_list, min_length=18):
+def remove_short_items(text_list: list[str], min_length=18):
     # 지정된 길이보다 긴 항목만 선택
     return [item for item in text_list if len(item) > min_length]
 
@@ -101,7 +133,7 @@ def remove_short_items(text_list, min_length=18):
 # 한글 문자 and 문자($)가 없는 항목 삭제
 
 
-def remove_non_korean_items(text_list):
+def remove_non_korean_items(text_list: list[str]):
     # 한글 문자를 포함하는 항목만 선택
     korean_pattern = re.compile('[$가-힣]')
     return [item for item in text_list if korean_pattern.search(item)]
