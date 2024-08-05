4a130f7df02f248f25d27ece744ba027f3183099
rosetteer-kim <rosetteer@gmail.com>
2024-08-06 05:48:23 +0900

    선지 추출 및 문제 형식(객관식, 주관식) 구별

--- home.py
+++ home.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import split_mathpix_tex_source as smts
+import modify_latex_scripts.letter_of_choice as ml
 
 
 def main():
     st.title("Mathpix TeX Source Spliter")
 
-    questions_file = st.file_uploader("TeX File: 문제", type="tex")
+    questions_file = st.file_uploader("TeX File: 반드시 문제", type="tex")
     solutions_file = st.file_uploader("TeX File: 해설", type="tex")
     if questions_file is not None and solutions_file is not None:
         questions = questions_file.getvalue().decode("utf-8")
@@ -14,13 +15,17 @@ def main():
             # tex file 분리
             questions_split = smts.split_mathpix_tex_source(questions)
             solutions_split = smts.split_mathpix_tex_source(solutions)
+            # 선지 추출 및 문제 형식(객관식, 주관식) 구별
+            mod_loc_q_split = ml.modify_letter_of_choice(questions_split)
 
             st.write(f"문제 수: {len(questions_split)}, 해설 수: {len(solutions_split)}")
 
             st.markdown("---")  # 구분선 추가
-            for i, content in enumerate(questions_split):
-                with st.expander(f"문제 {i+1}"):
-                    st.text(content)
+            for i, content in enumerate(mod_loc_q_split):
+                with st.expander(f"문제 {i + 1}"):
+                    st.text(content[0])
+                with st.expander(f"형태: {content[2]}"):
+                    st.text(content[1])
             st.markdown("---")  # 구분선 추가
             for i, content in enumerate(solutions_split):
                 with st.expander(f"해설 {i+1}"):
---
+++ modify_latex_scripts/letter_of_choice.py
@@ -0,0 +1,56 @@
+import re
+from typing import Any, LiteralString
+
+
+def sub_letter_of_choice(text: str, pattern: str) -> tuple[str, list[LiteralString | Any]]:
+
+    choice = []
+
+    matches = re.findall(pattern, text, re.DOTALL)
+    if matches:
+        for match in matches[0]:
+            if re.findall(r"[ㄱㄴㄷ가~힣]", match, re.DOTALL):
+                choice.append(match)
+            else:
+                choice.append('$'+match+'$')
+
+    text_modified = re.sub(pattern, '\n'+'-'*10+' 선지 위치 '+'-'*10+'\n', text)
+    
+    return text_modified, choice
+
+
+def letter_of_choice(text: str) -> tuple[str, list[LiteralString | Any], str]:
+
+    pattern1 = (r"(?m)\\\(\\begin.*"
+                r"\\text\s*{\s*\(1\)\s*}\s*(?:\\\()*\s*(.*?)\s*(?:\\\))*\s*&\s*"
+                r"\\text\s*{\s*\(2\)\s*}\s*(?:\\\()*\s*(.*?)\s*(?:\\\))*\s*&\s*"
+                r"\\text\s*{\s*\(3\)\s*}\s*(?:\\\()*\s*(.*?)\s*(?:\\\))*\s*&\s*"
+                r"\\text\s*{\s*\(4\)\s*}\s*(?:\\\()*\s*(.*?)\s*(?:\\\))*\s*&\s*"
+                r"\\text\s*{\s*\(5\)\s*}\s*(?:\\\()*\s*(.*?)\s*(?:\\\))*(?:\s*\\end{array}\\\))")
+    pattern2 = (r"(?m)^\(1\)\s*(?:\\\()*\s*(.*?)(?:\\\))*\s*\\\\\n+"
+                r"^\(2\)\s*(?:\\\()*\s*(.*?)(?:\\\))*\s*\\\\\n+"
+                r"^\(3\)\s*(?:\\\()*\s*(.*?)(?:\\\))*\s*\\\\\n+"
+                r"^\(4\)\s*(?:\\\()*\s*(.*?)(?:\\\))*\s*\\\\\n+"
+                r"^\(5\)\s*(?:\\\()*\s*(.*?)(?:\\\))*\s*(?=\s*\n|\Z)")
+
+    form_of_exercise = '객관식'
+
+    text_modified = sub_letter_of_choice(text, pattern1)
+    if not text_modified[1]:
+        text_modified = sub_letter_of_choice(text, pattern2)
+
+    if not text_modified[1]:
+        form_of_exercise = '주관식'
+
+    return text_modified[0], text_modified[1], form_of_exercise
+
+
+def modify_letter_of_choice(contents: list[str]) -> list[tuple[str, list[Any], str]]:
+    contents_modified = []
+
+    for content in contents:
+        content.join('\n')
+        content = re.sub(r'\n+', '\n', content)
+        contents_modified.append(letter_of_choice(content))
+
+    return contents_modified
